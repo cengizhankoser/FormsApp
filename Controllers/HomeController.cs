1ab7d5dd21fe1f4ca2b67270e1a12e7e97969864
Cengizhan Koser <cengizhankoserr@gmail.com>
2024-10-17 15:50:52 +0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using FormsApp.Models;
+
+namespace FormsApp.Controllers;
+
+public class HomeController : Controller
+{
+
+    public HomeController()
+    {
+    }
+
+    public IActionResult Index(string SearchString)
+    {
+        var products = Repository.Products;
+
+        if(!String.IsNullOrEmpty(SearchString))
+        {
+            ViewData["SearchString"] = SearchString;
+            products = products.Where(p => p.Name.IndexOf(SearchString, StringComparison.OrdinalIgnoreCase) >= 0).ToList();
+
+
+        }
+        return View(products);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+}
